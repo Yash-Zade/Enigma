c8cbc54fc7321cb3aaeb0ed56edd0583e17d2296
arpitSatpute <arpitrameshsatpute6986@gmail.com>
2025-02-13 11:22:51 +0530

    WebsocketConfig and Handlers

---
+++ Backend/src/main/java/com/teamarc/demo/handlers/SignalingHandler.java
@@ -0,0 +1,32 @@
+package com.teamarc.demo.handlers;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SignalingHandler extends TextWebSocketHandler {
+
+    private ConcurrentHashMap<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.put(session.getId(), session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        for (WebSocketSession s : sessions.values()) {
+            if (s.isOpen() && !s.getId().equals(session.getId())) {
+                s.sendMessage(message);
+            }
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session.getId());
+    }
+}
